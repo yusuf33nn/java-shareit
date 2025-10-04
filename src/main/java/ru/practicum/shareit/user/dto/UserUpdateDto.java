f8450cc7f0dd1a742af5ba65faa44b3bb800e01b
y_muradov <surgeon33nn@yandex.ru>
2025-10-04 16:51:02 +0300

    fix checkstyle

--- src/main/java/ru/practicum/shareit/user/dto/UserUpdateDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserUpdateDto.java
@@ -2,8 +2,6 @@ package ru.practicum.shareit.user.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
