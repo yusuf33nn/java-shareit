4faad4419b70713214a0c81183740e79c27ebb80
y_muradov <surgeon33nn@yandex.ru>
2025-10-15 22:31:42 +0300

    fix checkstyle

--- server/src/main/java/ru/practicum/shareit/request/controller/ItemRequestController.java
+++ server/src/main/java/ru/practicum/shareit/request/controller/ItemRequestController.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.request.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
